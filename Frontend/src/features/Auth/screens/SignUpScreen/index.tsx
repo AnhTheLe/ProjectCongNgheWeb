61d6ae072e86d78d84fa82a0a3c5110522ad0898
quynhanh13 <120185029+quynhanh13@users.noreply.github.com>
2023-10-22 19:13:16 +0700

    Update README.md

--- Frontend/src/features/Auth/screens/SignUpScreen/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-export const SignUpScreen = () => {
-  return <div>SignUpScreen</div>;
-};
--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,2 @@
-# Hospital Website
+# Công Nghệ Web
+
