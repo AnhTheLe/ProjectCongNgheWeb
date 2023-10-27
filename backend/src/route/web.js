0c6aa9e699d33cbadbb6f4371ca518812e7c5cf4
unknown <leducanh26102002@gmail.com>
2023-10-28 00:11:12 +0700

    'add_api'

--- backend/src/route/web.js
+++ backend/src/route/web.js
@@ -1,13 +1,43 @@
-import express from "express";
+import express, { request } from "express";
 
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
+    router.post('/todos',(request,respond)=>{
+        //  tao moi row trong database
+        respond.json("hello world");
+    });
     
+    router.get('/todos',(request,respond)=>{
+        // get du lieu tu database
+        response.json({message:'all todos'})
+    });
+    
+    router.put('/todos',(request,respond)=>{
+        // chinh sua databae
+    });
+    
+    router.delete('/todos',(request,respond)=>{
+        // delete la xoa 1 dong trong database
+    });
 
 
     return app.use("/", router);
 }
+// router.post('/todos',(request,respond)=>{
 
+// });
+
+// router.get('/todos',(request,respond)=>{
+
+// });
+
+// router.put('/todos',(request,respond)=>{
+
+// });
+
+// router.delete('/todos',(request,respond)=>{
+
+// });
 module.exports = initWebRoutes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,21 +7,25 @@ import cors from 'cors';
 
 require('dotenv').config();
 
-let app = express();
+let app = express();    
 
 //config app
 app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use('/api',initWebRoutes)
 configViewEngine(app);
 initWebRoutes(app);
 
 connectDB();
 
-let port = process.env.PORT || 6969;
+let port = process.env.PORT || 8080;
 //port == undefined => port = 6969
 
 app.listen(port, () => {
     console.log("Web is running on the port: " + port)
 });
+
+
+
+
