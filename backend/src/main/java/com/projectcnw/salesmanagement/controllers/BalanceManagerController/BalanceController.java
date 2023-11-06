6e17833958eb6d49bb8f60d0d00501684503e069
AnhLD2610 <leducanh26102002@gmail.com>
2023-11-07 01:00:30 +0700

    Merge branch 'backend' into 20204628-ApiProduct
    # Conflicts:
    #	backend/src/main/java/com/projectcnw/salesmanagement/repositories/OrderRepositories/OrderLineRepository.java
    #	backend/src/main/java/com/projectcnw/salesmanagement/repositories/OrderRepositories/OrderRepository.java
    #	backend/src/main/java/com/projectcnw/salesmanagement/repositories/OrderRepositories/ReturnOrderLineRepository.java
    #	backend/src/main/java/com/projectcnw/salesmanagement/repositories/OrderRepositories/ReturnOrderRepository.java
    
    Revert "add order repository"
    
    This reverts commit 8f77c47d2690dd7cd7670c89f56668ca00a2421a.

--- backend/src/main/java/com/projectcnw/salesmanagement/controllers/BalanceManagerController/BalanceController.java
+++ backend/src/main/java/com/projectcnw/salesmanagement/controllers/BalanceManagerController/BalanceController.java
@@ -10,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
--- backend/src/main/java/com/projectcnw/salesmanagement/repositories/OrderRepositories/ReturnOrderLineRepository.java
+++
@@ -1,9 +0,0 @@
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReturnOrderLineRepository extends JpaRepository<ReturnOrderLine, Integer> {
-
-    List<ReturnOrderLine> findAllByReturnOrderId(Integer orderId);
-}
