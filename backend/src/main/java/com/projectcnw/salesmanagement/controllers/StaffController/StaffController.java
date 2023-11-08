7d30627f5cf8bae5dd58d2cbaaed9ef88dd0907d
quynhanh13 <quynhanh1392002@gmail.com>
2023-11-08 12:22:16 +0700

    add api create staff

--- backend/src/main/java/com/projectcnw/salesmanagement/controllers/StaffController/StaffController.java
+++ backend/src/main/java/com/projectcnw/salesmanagement/controllers/StaffController/StaffController.java
@@ -2,6 +2,8 @@ package com.projectcnw.salesmanagement.controllers.StaffController;
 
 import com.projectcnw.salesmanagement.dto.PagedResponseObject;
 
+import com.projectcnw.salesmanagement.dto.ResponseObject;
+import com.projectcnw.salesmanagement.dto.staff.CreateStaffDto;
 import com.projectcnw.salesmanagement.dto.staff.StaffItemDto;
 import com.projectcnw.salesmanagement.services.StaffService.StaffService;
 import jakarta.validation.Valid;
@@ -37,5 +39,16 @@ public class StaffController {
                 .responseCode(200)
                 .build());
     }
+
+    @PostMapping
+    public ResponseEntity<ResponseObject> createStaff(@RequestBody @Valid CreateStaffDto createStaffDto) {
+        staffService.createStaff(createStaffDto);
+        return ResponseEntity.ok(ResponseObject.builder()
+                .message("success")
+                .responseCode(200)
+                .build());
+    }
+
+
 
 }
--- backend/src/main/java/com/projectcnw/salesmanagement/services/StaffService/StaffService.java
+++ backend/src/main/java/com/projectcnw/salesmanagement/services/StaffService/StaffService.java
@@ -2,18 +2,25 @@ package com.projectcnw.salesmanagement.services.StaffService;
 
 
 import com.projectcnw.salesmanagement.dto.staff.StaffItemDto;
-
+import com.projectcnw.salesmanagement.dto.staff.CreateStaffDto;
+import com.projectcnw.salesmanagement.exceptions.NotFoundException;
+import com.projectcnw.salesmanagement.models.Role;
 import com.projectcnw.salesmanagement.models.UserEntity;
 
+import com.projectcnw.salesmanagement.models.enums.RoleType;
+import com.projectcnw.salesmanagement.models.enums.WorkStatus;
 import com.projectcnw.salesmanagement.repositories.UserRepository;
+import com.projectcnw.salesmanagement.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,6 +30,10 @@ public class StaffService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public long countTotalStaffs() {
         return userRepository.count() - 1;
@@ -36,4 +47,17 @@ public class StaffService {
         return Arrays.asList(modelMapper.map(staffList, StaffItemDto[].class));
     }
 
+    public void createStaff(CreateStaffDto createStaffDto) {
+        createStaffDto.setPassword(passwordEncoder.encode(createStaffDto.getPassword()));
+        ModelMapper modelMapper = new ModelMapper();
+        List<Role> roles = new ArrayList<>();
+        for (RoleType roleName: createStaffDto.getRoleNames()) {
+            Role role = roleRepository.findByName(roleName).orElseThrow(() -> new NotFoundException("role not found"));
+            roles.add(role);
+        }
+        UserEntity user = modelMapper.map(createStaffDto, UserEntity.class);
+        user.setWorkStatus(WorkStatus.WORKING);
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
 }
