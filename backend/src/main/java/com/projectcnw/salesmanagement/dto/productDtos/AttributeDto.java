3dad2bfeaaa5ca5c12ba0599c994634b0d7e9cab
AnhLD2610 <leducanh26102002@gmail.com>
2023-11-08 20:46:22 +0700

    add api quản lý sản phẩm

---
+++ backend/src/main/java/com/projectcnw/salesmanagement/dto/productDtos/AttributeDto.java
@@ -0,0 +1,12 @@
+package com.projectcnw.salesmanagement.dto.productDtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AttributeDto {
+    private String keyAttribute;
+    private String name;
+    private String value;
+}
