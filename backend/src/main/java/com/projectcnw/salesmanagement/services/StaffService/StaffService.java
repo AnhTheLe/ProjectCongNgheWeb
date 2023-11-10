043a3a268cc81a95fe1a5b16e16e2b14c0608b18
quynhanh13 <quynhanh1392002@gmail.com>
2023-11-10 17:36:01 +0700

    add config sercurity

--- backend/src/main/java/com/projectcnw/salesmanagement/services/StaffService/StaffService.java
+++ backend/src/main/java/com/projectcnw/salesmanagement/services/StaffService/StaffService.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
