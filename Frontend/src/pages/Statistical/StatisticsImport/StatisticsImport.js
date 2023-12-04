761d19955c7612533368949f9895a58570be2558
AnhLD2610 <leducanh26102002@gmail.com>
2023-12-04 13:07:23 +0700

    Add StatisticsImport

---
+++ Frontend/src/pages/Statistical/StatisticsImport/StatisticsImport.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames/bind';
+import styles from './StatisticsImport.module.scss';
+
+const cx = classNames.bind(styles);
+
+function StatisticsImport() {
+    return (
+        <div>
+            Thống kê nhập hàng
+        </div>
+    );
+}
+
+export default StatisticsImport;
